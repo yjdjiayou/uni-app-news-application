8050effdc88083b7465bc9bc419b10e49819100e
yjd <674032206@qq.com>
2020-08-14 16:54:32 +0800

    详情页-内容展示&评论发布&回复

--- common/js/router-tree.js
+++ common/js/router-tree.js
@@ -5,7 +5,8 @@
 export const routerTree = {
 	home: '/pages/tabbar/home/home', // 首页
 	mine: '/pages/tabbar/mine/mine', // 个人中心
-	homeLabel:'/pages/home-label/home-label',// 首页-标签页
-	homeSearch:'/pages/home-search/home-search',// 首页-搜索页
-	homeDetail:'/pages/home-detail/home-detail',// 首页-详情页
+	homeLabel:'/pages/home-label/home-label',// 标签页
+	homeSearch:'/pages/home-search/home-search',// 搜索页
+	homeDetail:'/pages/home-detail/home-detail',// 详情页
+	detailComments:'/pages/detail-comments/detail-comments',// 详情页-评论
 };
