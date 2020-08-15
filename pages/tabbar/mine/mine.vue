3da75c6c710e548ad7b8712a46716328f5db2d87
yjd <674032206@qq.com>
2020-08-15 17:01:54 +0800

    新增我的页面静态布局

--- pages/tabbar/mine/mine.vue
+++ pages/tabbar/mine/mine.vue
@@ -1,6 +1,49 @@
 <template>
 	<view>
-		我的
+		<view class="my-header">
+			<view class="my-header__bg">
+				<image src="/static/logo.png" mode="aspectFill"></image>
+			</view>
+
+			<view class="my-header__logo">
+				<view class="my-header__logo-box">
+					<image src="/static/logo.png" mode="aspectFill"></image>
+				</view>
+				<text class="my-header__name">aaa</text>
+			</view>
+
+			<view class="my-header__info">
+				<view class="my-header__info-box">
+					<text class="my-header__info-title">被关注</text>
+					<text class="my-header__name">aaa</text>
+				</view>
+				<view class="my-header__info-box">
+					<text class="my-header__info-title">粉丝</text>
+					<text class="my-header__name">aaa</text>
+				</view>
+				<view class="my-header__info-box">
+					<text class="my-header__info-title">积分</text>
+					<text class="my-header__name">aaa</text>
+				</view>
+			</view>
+
+		</view>
+
+
+		<view class="my-content">
+			<view class="my-content__list">
+				<view class="my-content__list-title">
+					<uni-icons class="icons" type="contact" size="14" color="#666"></uni-icons>
+					<text>我的文章</text>
+				</view>
+			</view>
+			<view class="my-content__list">
+				<view class="my-content__list-title">
+					<uni-icons class="icons"  type="help" size="14" color="#666"></uni-icons>
+					<text>意见反馈</text>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,15 +51,103 @@
 	export default {
 		data() {
 			return {
-				
+
 			}
 		},
 		methods: {
-			
+
 		}
 	}
 </script>
 
-<style>
+<style lang="scss">
+	page {
+		background-color: #f5f5f5;
+	}
 
+	.my-header {
+		position: relative;
+		padding-bottom: 15px;
+
+
+		.my-header__bg {
+			position: absolute;
+			left: 0;
+			right: 0;
+			top: 0;
+			bottom: 0;
+			filter: blur(8px);
+			opacity: 0.3;
+
+			image {
+				width: 100%;
+				height: 100%;
+			}
+		}
+
+		.my-header__logo {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			padding-top: 30px;
+
+			.my-header__logo-box {
+				border-radius: 50%;
+				width: 60px;
+				height: 60px;
+				overflow: hidden;
+
+				image {
+					width: 100%;
+					height: 100%;
+				}
+			}
+
+			.my-header__name {
+				margin-top: 15px;
+				font-size: 16px;
+				font-weight: bold;
+			}
+		}
+
+		.my-header__info {
+			display: flex;
+			margin-top: 15px;
+
+			.my-header__info-box {
+				display: flex;
+				flex-direction: column;
+				justify-content: center;
+				align-items: center;
+				width: 100%;
+				font-size: 12px;
+				color: #999;
+
+				.my-header__info-title {
+					font-size: 14px;
+					color: #333;
+				}
+			}
+		}
+	}
+
+	.my-content {
+		.my-content__list {
+			display: flex;
+			justify-content: space-between;
+			margin-bottom: 10px;
+			padding: 15px;
+			background-color: #fff;
+			color: #333;
+			font-size: 14px;
+
+			.my-content__list-title {
+				display: flex;
+				align-items: center;
+				.icons{
+					margin-right: 5px;
+				}
+			}
+		}
+	}
 </style>
